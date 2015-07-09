2f37c07371372b6492beef5a677fef7bbace6548
Brian Pan <b97705021@ntu.edu.tw>
2015-07-09 16:47:02 +0800

    change host

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,3 +1,3 @@
 email_setting = YAML::load(File.open(File.join(Rails.root, "config", "email.yml")))
 ActionMailer::Base.smtp_settings = email_setting[Rails.env]
-ActionMailer::Base.default_url_options[:host] = "http://localhost:3000"
+ActionMailer::Base.default_url_options[:host] = Figaro.env.host
